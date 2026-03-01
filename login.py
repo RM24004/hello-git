8a44ae7a1fb18ff3f1c8aea0d9ded5b0e3219bdf
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-02-28 22:15:22 -0600

    Agregando el menu V1

--- login.py
+++ login.py
@@ -6,6 +6,16 @@ while True:
     contraseña = input()    
     if usuario == "admin" and contraseña == "admin123":
         print("Bienvenido, " + usuario + "!")
+        print("Has iniciado sesión correctamente.")
+        print("Ahora puedes acceder a las funciones del programa.")
+        print("El progrma esta en desarrollo, por lo que no hay funciones disponibles por el momento.")
+        print("Menu de funciones:")
+        print("1. Función 1")
+        print("2. Función 2")   
+        print("3. Función 3")
+        print("4. Salir")
+        print("Selecciona una opción:")
+        seleccion = input()
         break
     else:
         print("Usuario o contraseña incorrectos. Inténtalo de nuevo.")      
