88812ba1a352d157c069eaeed60089599b901672
RM24004 <167376455+RM24004@users.noreply.github.com>
2026-02-28 22:31:56 -0600

    commit del cambio en gitpy 2 desde la main

--- hellogit2.py
+++ hellogit2.py
@@ -1,3 +1,4 @@
 print ("hello git 2. py")
 print("vamos a crear tags en git y a crear ramas en git")
 print("agregamos mas codigo en la rama main sin afectar la rama login")
+print ("Nos dara conflicto con la otra rama login, pero lo resolveremos con git merge")
